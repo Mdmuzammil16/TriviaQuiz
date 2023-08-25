2849c15bc1dc772586533f2f78e7621c39145e1e
mohammedmuzammil <fazil8785@gmail.com>
2023-08-25 15:30:49 +0530

    new test

--- app/src/main/java/com/trivia/quiz/Fragment/CategoryFragment.kt
+++ app/src/main/java/com/trivia/quiz/Fragment/CategoryFragment.kt
@@ -18,10 +18,10 @@ import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class CategoryFragment: Fragment() {
+class CategoryFragment : Fragment() {
 
-    var _binding: FragmentCategoryBinding? = null
-    val binding get() = _binding!!
+    private var _binding: FragmentCategoryBinding? = null
+    private val binding get() = _binding!!
 
     @Inject
     lateinit var userPreference: UserPreference
@@ -30,14 +30,11 @@ class CategoryFragment: Fragment() {
     lateinit var musicClass: MusicClass
 
 
-
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentCategoryBinding.inflate(inflater, container,false)
+        _binding = FragmentCategoryBinding.inflate(inflater, container, false)
 
         return binding.root
     }
@@ -46,20 +43,29 @@ class CategoryFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
 
-
         val list = arrayListOf<CategoryModel>(
-            CategoryModel("Arts & Literacy", "" +
-                    "Test Your Knowledge in Arts & Literacy", R.drawable.arts,"arts_and_literature"),
-            CategoryModel("Geography", "Let's Explore more about our geographical Knowledge",
-                R.drawable.geo, "geography"),
-            CategoryModel("Music", "" +
-                "Test Your Knowledge in Music", R.drawable.music,"music"),
-            CategoryModel("Sports", "" +
-                    "Test Your Knowledge in Sports", R.drawable.sports,"sport_and_leisure"),
-            CategoryModel("Food", "" +
-                    "Test Your Knowledge in Food", R.drawable.food,"food_and_drink"),
-            CategoryModel("Flims", "" +
-                    "Test Your Knowledge in Flims", R.drawable.flim,"film_and_tv")
+            CategoryModel(
+                "Arts & Literacy",
+                "" + "Test Your Knowledge in Arts & Literacy",
+                R.drawable.arts,
+                "arts_and_literature"
+            ), CategoryModel(
+                "Geography",
+                "Let's Explore more about our geographical Knowledge",
+                R.drawable.geo,
+                "geography"
+            ), CategoryModel(
+                "Music", "" + "Test Your Knowledge in Music", R.drawable.music, "music"
+            ), CategoryModel(
+                "Sports",
+                "" + "Test Your Knowledge in Sports",
+                R.drawable.sports,
+                "sport_and_leisure"
+            ), CategoryModel(
+                "Food", "" + "Test Your Knowledge in Food", R.drawable.food, "food_and_drink"
+            ), CategoryModel(
+                "Flims", "" + "Test Your Knowledge in Flims", R.drawable.flim, "film_and_tv"
+            )
 
         )
 
@@ -78,8 +84,8 @@ class CategoryFragment: Fragment() {
     override fun onStart() {
         super.onStart()
 
-        binding.pointsTv.text = userPreference.getUserinfo("points","0")
-        binding.levelTv.text = "Lvl: ${userPreference.getUserinfo("level","0")}"
+        binding.pointsTv.text = userPreference.getUserinfo("points", "0")
+        binding.levelTv.text = "Lvl: ${userPreference.getUserinfo("level", "0")}"
         binding.profileIv.setImageResource(userPreference.getUserinfo("image").toInt())
         binding.nameTv.text = userPreference.getUserinfo("name", "")
     }
